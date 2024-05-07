9be7f5e75a7b5ba89db928e0fe3a1b1b59c11127
AdanAamir <adanaamir2005@gmail.com>
2024-05-08 02:19:15 +0500

    adans first successful commit

--- mycode.cpp
+++ mycode.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout << "iam writing a code linked witb git";
+    cout << "iam writing a code linked witb git";uff
     //iam changing this code
 }
